89feabdff4b27622ce94dda7b885775a3a8dd8ca
Murtazayldz <37036516+Murtazayldz@users.noreply.github.com>
2018-03-20 01:39:13 +0300

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,25 @@
+#tsm:Toplam satış miktarı
+#hm:Hammadde maliyeti
+#bog:Bakım onarım giderleri
+#sg:Sevkiyat giderleri
+#sahg:Satın alınan hizmet giderleri
+#kdc:Katma Değer Ciro
+print("Katma Değer Ciro Hesaplama")
+def katmaDegerCiro(tsm,hmm,bkog,svg,sah):
+    global kdc
+    kdc=tsm-(hm+bog+sg+sahg)
+    if kdc>1000:
+        print("işletme katma değer cirosu yüksek.",kdc)
+    elif 500<kdc<999:
+        print("işletme katma değer cirosu normal.",kdc)
+    elif kdc<500:
+        print("katma değer cirosu düşük.",kdc)
+    return kdc
+tsm=int(input("Toplam satış miktarı giriniz:"))
+hm=int(input("Hammadde maliyeti giriniz:"))
+bog=int(input("Bakım onarım giderlerinizi giriniz:"))
+sg=int(input("Sevkiyat giderlerini giriniz:"))
+sahg=int(input("Satın alınan hizmet giderleriniz:"))
+
+katmaDegerCiro=katmaDegerCiro(tsm,hm,bog,sg,sahg)
+
