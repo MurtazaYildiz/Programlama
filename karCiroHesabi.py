c03dbb4c124381eda6dab3767bbb534f24ed1ceb
Murtazayldz <37036516+Murtazayldz@users.noreply.github.com>
2018-04-27 14:38:29 +0300

    Modulsoru1

---
+++ karCiroHesabi.py
@@ -0,0 +1,9 @@
+def kar(gelir,gider):
+    gelir=int(gelir)
+    gider=int(gider)
+    print("toplam iþletme karý:",gelir-gider)
+
+def adamBasiCiro(toplamCiro,toplamCalisan):
+    toplamCiro=int(toplamCiro)
+    toplamCalisan=int(toplamCalisan)
+    print("Adam Baþý Ciro:",toplamCiro/toplamCalisan)
